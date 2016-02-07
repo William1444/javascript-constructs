c38cfac174112cbed24c959c3585fa576ec5b51e
William Lacy <williamlacy@Williams-MacBook-Air.local>
2016-02-07 15:33:08 +0000

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# javascript-constructs
+
+This repo is a library of constructs for javascript. Constructs are a part of every popular framework. They each come with
+ their benefits and drawbacks from a performance, scalability and usability
+
+# test/run
+
+The test framework is used to describe the constructs, so is in essence the application runtime as well.
+
+To serve the project execute `grunt serve`.
--- public/specs/Recursion.js
+++ public/specs/Recursion.js
@@ -1,39 +1,23 @@
-describe('#factorial',function(){
-	it('should return 6 for 3!',function(){
-		expect(factorial(3)).toBe(6)
-		expect(factorial2(3)).toBe(6)
-	});
-	it('should return 2 for 2!',function(){
-		expect(factorial(2)).toBe(2)
-		expect(factorial2(2)).toBe(2)
-	});
-	it('should return 24 for 4!',function(){
-		expect(factorial(4)).toBe(24);
-		expect(factorial2(4)).toBe(24);
-	});
+describe('#factorial', function () {
+    it('should return 6 for 3!', function () {
+        expect(factorial(3)).toBe(6)
+    });
+    it('should return 2 for 2!', function () {
+        expect(factorial(2)).toBe(2)
+    });
+    it('should return 24 for 4!', function () {
+        expect(factorial(4)).toBe(24);
+    });
 });
 
-function factorial(num){
-	var factorialRes = 1;
-	(function factor(n){
-		if (n === 1){
-			return factorialRes;
-		} else {
-			factorialRes *= n;
-			factor(n-1);
-		}		
-	})(num);
-	return factorialRes;
-}
-
-function factorial2(num){
-	var res = 1;
-	return (function factor(n){
-		if (n === 1) {
-			return res;
-		} else {
-			res *= n;
-			return factor(n-1);
-		}
-	})(num);
+function factorial2(num) {
+    var res = 1;
+    return (function factor(n) {
+        if (n === 1) {
+            return res;
+        } else {
+            res *= n;
+            return factor(n - 1);
+        }
+    })(num);
 }
