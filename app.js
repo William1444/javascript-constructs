0f7e84ffcc98f5210fc3a23104ae642d23d318c4
williamlacy <william.lacy@hotmail.co.uk>
2016-02-07 15:41:54 +0000

    fix reporter issue in html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public'));
+app.use(express.static('public'));
 app.use('/bower_components', express.static('bower_components'));
 
 app.listen(3000, function () {
